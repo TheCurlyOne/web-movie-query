07de974e0b83585e4fc546a38efba2dbf49375f8
TheCurlyOne <sargent.null@gmail.com>
2023-02-25 15:12:41 -0800

    initial page setup with simple front-end and incomplete back-end

---
+++ webapp/pages/index.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+
+    // Return movie list to display
+    data() {
+        return {
+           movieList: ''
+        }
+    },
+
+    // The method used to call the backend search
+    methods: {
+        searchMovies() {
+           movieList = backend.movieSearch(movieSubject);
+        }
+    },
+
+    // Include backend script
+    mounted() {
+        let backendScript = document.createElement('script')
+        backendScript.setAttribute('src', '../webservice/backendSearch.js')
+        document.head.appendChild(backendScript)
+        console.log(`The page has been loaded`)
+    }
+}
+</script>
+
+<template>
+    <input v-model="movieSubject" placeholder="Movie subject" />
+    <button @click="searchMovies">Search</button>
+    <p>Your movie list</p>
+    <p> {{ movieList }}</p>
+</template>
---
+++ webservice/backendSearch.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+// URL to access TMDB
+webUrl = 'https://api.themoviedb.org/3/search/movie?';
+apiKey = 'api_key=97a5d09107febb99e4110eefe87dd826';
+language = '&language=en-US&query=';
+begining = webUrl + apiKey + language;
+end = '&page=1&include_adult=false';
+
+// Function to take movie search term
+exports.movieSearch = function (movieSubject) {
+  // Build query to supply movie subject to TMDB
+  fullWebString = begining + movieSubject + end;
+
+  // - Edit list of returned movies -
+
+  // Only include the first 10 movies (if less than 10 include all)
+
+  // Parse movie arrar to include desired content
+  //   id,title,poster_path,popularity
+
+  // -
+
+  // Return movie list
+  return movieListBackend;
+};
+
+
